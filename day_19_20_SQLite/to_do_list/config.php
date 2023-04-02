7a987d9c5bbe03724fe686747e8468d974500e7f
craignyu <kapp@cs.nyu.edu>
2023-04-02 08:37:12 -0400

    Days 19 20

---
+++ day_19_20_SQLite/to_do_list/config.php
@@ -0,0 +1,5 @@
+<?php
+  // update this with your own file path!!!!
+  //$path = '/path/to/your/database/folder/should/go/here';
+  $path = '/Users/craigkapp/Documents/MAMP/nyu_csci_us_0061_source_code_spring_2023/day_19_20_SQLite/databases';
+?>
